88b20afcc6be30ca5de423f4ffcf028095d3af7b
drewmania <justinhot906@gmail.com>
2023-06-27 22:26:25 -0400

    add code for send data to xlsx file

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ const express = require('express')
 const app = express()
 app.listen(3000, e=> console.log('listening at port 3000'))
 app.use(express.static('public'))
+
+// Get xlsx file
+const workbook = xlsx.readFile('Student most like classes.xlsx')
+const worksheets = {}
+
+// fill out "worksheets"
+for(const sheetName of workbook.SheetNames) {
+    worksheets[sheetName] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName])
+}
+
+// Post to excel
+app.use(express.json({ limit: '1mb' }))
+app.post('/pushXlsx', (req, res) => {
+    console.log('I got a request')
+    let data = req.body
+    console.log(data)
+
+    // Modify XLSX
+    worksheets['Sheet1'].push({
+        'Student Name': data['name'],
+        'Math': data['math'],
+        'Science': data['science'],
+        'History': data['history'],
+        'ELA': data['ela']
+    })
+
+    // update xlsx file
+    xlsx.utils.sheet_add_json(workbook.Sheets['Sheet1'], worksheets['Sheet1'])
+    xlsx.writeFile(workbook, "Student most like classes.xlsx")
+})
+
